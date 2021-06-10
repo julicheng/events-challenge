3284d62f246a72bb1433584cf5334d879a81a2e2
Juli Cheng <juli.cheng@axiell.com>
2021-06-10 12:35:32 +0100

    Add icons and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Work Sans', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
-    extend: {},
+    opacity: ({ after }) => after(['disabled']),
   },
   plugins: [],
 };
